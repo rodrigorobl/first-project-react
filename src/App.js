89ad057e6f4f29db367ce5181634667de4cb1688
rodrigorobl <rodrigo.augusto.r21@gmail.com>
2024-02-21 23:46:47 -0300

    users

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import Reac from "react"
+import Reac from "react";
+
+import People from './assets/people.svg'
+import Arrow from './assets/arrow.svg'
+
+import {
+  Container,
+  H1,
+  Image,
+  ContainerItens,
+  InputLabel,
+  Input,
+  Button
+} from './styles';
 
 const App = () => {
+  return (
+    <Container>
+      <Image alt="logo-imagem" src={People} />
+      <ContainerItens>
+        <H1>Olá!</H1>
+        <InputLabel>Nome</InputLabel>
+        <Input placeholder="Nome" />
 
-  return <div>Hello World</div>
+        <InputLabel>Idade</InputLabel>
+        <Input placeholder="Idade" />
 
+        <Button>Cadastrar <img alt="seta" src={Arrow} /></Button>
+
+      </ContainerItens>
+    </Container>
+  );
 }
 
-export default App
+export default App;
